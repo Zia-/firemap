47a2b94a5863f045f77e59ebe0e178a7f0c2e07e
Mohammed Zia <mohammed.zia33@gmail.com>
2016-05-20 01:47:18 +0300

    buttons func

--- js/documentLoad.js
+++ js/documentLoad.js
@@ -1,24 +1,14 @@
 $( document ).ready(function() {
-  // $("#frp-pixel_seviri").click(function () {
-  //  $(this).toggleClass("green");
-  // });
-  // $("#frp-pixel_seviri").click(function(){
-  //     $(this).toggle(
-  //       function(){alert("aa")},
-  //       function(){alert("bb")}
-  //     );
-  // });
-  // $('#frp-pixel_seviri').toggle(
-  //   function() {
-  //     alert("aa")
-  //     // $(this).animate({
-  //     //     width: "260px"
-  //     // }, 1500);
-  //   },
-  //   function() {
-  //     alert("bb")
-  //     // $(this).animate({
-  //     //     width: "30px"
-  //     // }, 1500);
-  //   });
+  // Following is green blinking light
+  var testerVar = 0;
+  function myTimer() {
+    if (testerVar == 0){
+      $(".led-green").css('background-color','transparent');
+      testerVar = 1;
+    } else {
+      $(".led-green").css('background-color','#55FF00');
+      testerVar = 0;
+    }
+  };
+  var sirenTimer = setInterval(myTimer, 500);
 });
